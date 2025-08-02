93904b474bda295065c8d6b5670aec36defb9106
qin en <qinentoh@gmail.com>
2025-08-02 14:25:47 +0800

    Initial Commit

---
+++ Bobby/BobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobbyApp.swift
+//  Bobby
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
